d935443fdea751e7a9aae17272381d621a9b4bb9
lauren garelick <laurengarelick@gmail.com>
2020-06-09 00:04:31 -0700

    building config folder

--- db/schema.sql
+++ db/schema.sql
@@ -4,8 +4,8 @@ USE taco_db;
 CREATE TABLE `tacos` (
   `id` Int( 11 ) AUTO_INCREMENT NOT NULL,
   `taco_type` VARCHAR( 255) NOT NULL,
-  `devoured` VARCHAR( 255 ) NOT NULL,
-  `created_at` DATETIME NOT NULL,
+  `devoured` BOOLEAN NOT NULL,
+  `guac` BOOLEAN NOT NULL,
 
   PRIMARY KEY ( `id` ) 
 );
---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO tacos (taco_type, devoured, guac)
+VALUES ("Carne Asada", TRUE, TRUE);
+
+INSERT INTO tacos (taco_type, devoured, guac)
+VALUES ("Spicy Carnitas", TRUE, FALSE);
+
+INSERT INTO tacos (taco_type, devoured, guac)
+VALUES ("Pollo", TRUE, TRUE);
+
+INSERT INTO tacos (taco_type, devoured, guac)
+VALUES ("Al Pastor", TRUE, FALSE);
